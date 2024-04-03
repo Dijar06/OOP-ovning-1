2a18bc6eb35108f57608cc2bba4bd67bfd667122
Dijar06 <dize06001@utb.vaxjo.se>
2024-04-03 10:56:50 +0200

    Add project files.

---
+++ OOP Övning 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_Övning_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 bil1 = new Class1("Audi", "RS6", 2024, 900, 300);
+            Console.WriteLine("Bil 1:");
+            Console.WriteLine("Märke: " + bil1.Märke);
+            Console.WriteLine("Modell: " + bil1.Modell);
+            Console.WriteLine("Årsmodell: " + bil1.Årsmodell);
+            Console.WriteLine("Hästkrafter: " + bil1.Hästkrafter);
+            Console.WriteLine("Maxhastighet: " + bil1.Maxhastighet);
+        }
+    }
+}
